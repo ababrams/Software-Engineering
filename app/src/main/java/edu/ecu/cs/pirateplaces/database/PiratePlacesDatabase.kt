28f1ed6bdbe239813736aaeab3937758e68a2c5b
ababrams <69653354+ababrams@users.noreply.github.com>
2021-11-29 17:56:12 -0500

    Initial commit

---
+++ app/src/main/java/edu/ecu/cs/pirateplaces/database/PiratePlacesDatabase.kt
@@ -0,0 +1,27 @@
+package edu.ecu.cs.pirateplaces.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import edu.ecu.cs.pirateplaces.PiratePlace
+
+@Database(entities = [ PiratePlace::class ], version = 2)
+@TypeConverters(PiratePlacesTypeConverters::class)
+abstract class PiratePlacesDatabase : RoomDatabase() {
+    abstract fun piratePlacesDao() : PiratePlacesDao
+}
+val migration_1_2 = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+       database.execSQL(
+            "ALTER TABLE PiratePlace ADD COLUMN hasVisited INTEGER NOT NULL DEFAULT 0"
+        )
+    database.execSQL(
+    "ALTER TABLE PiratePlace ADD COLUMN latitude REAL NOT NULL DEFAULT 0.00"
+    )
+     database.execSQL(
+      "ALTER TABLE PiratePlace ADD COLUMN longitude REAL NOT NULL DEFAULT 0.00"
+        )
+}
+}
